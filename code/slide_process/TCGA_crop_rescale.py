d065d1c154957d8fdb5f1d1565e85820cab6f2f5
lli289 <lli289.git@gmail.com>
2024-12-26 18:57:33 -0500

    update crop and rescale for TCGA use

--- code/slide_process/TCGA_crop_rescale.py
+++ code/slide_process/TCGA_crop_rescale.py
@@ -5,10 +5,23 @@ import argparse
 import pickle
 import cv2
 import openslide.deepzoom
+import pandas as pd
 
 from PIL import Image
 
-# Functions from cca package
+# Lillian 12/26
+# This code is used to rescale and crop TCGA histology images
+
+# Rescale part:
+# TCGA Histology images 2048 x 1536 pixel
+# BACH Histology images 2048 x 1536 pixel
+# BACH Pixel scale 0.42 µm x 0.42 µm
+# scale_factor = 0.42 / data['Scale.X'] e.g.,
+# each one divided by it's own scale, not just one fixed number!
+
+
+# Define functions part:
+    
 def stretch_pre(nimg):
     nimg = nimg.transpose(2, 0, 1)
     nimg[0] = np.maximum(nimg[0] - nimg[0].min(), 0)
@@ -42,37 +55,39 @@ def from_pil(pimg):
 
 def to_pil(nimg):
     return Image.fromarray(np.uint8(nimg))
-
-# Function to extract and process tiles
-def extract_tile_features(level, coord, zoom):
-    tile = np.array(zoom.get_tile(level, (coord[1], coord[2])))
-    tile = Image.fromarray(tile)
-    tile = to_pil(stretch(from_pil(tile)))
-    tile = np.array(tile)
-    return tile
-
+    
 def rescale_image(image, scale_factor):
     height, width = image.shape[:2]
-    new_height, new_width = int(height * scale_factor), int(width * scale_factor)
+    new_height, new_width = int(height / scale_factor), int(width / scale_factor) # Lillian edited on 12/16 divide not multiply
     return cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_LINEAR)
 
+def crop_image(image, height, width, strideH, strideW):
+    h, w = image.shape[:2]
+    tiles = []
+    for y in range(0, h, strideH):
+        if y + height > h:
+            y = h - height
+        for x in range(0, w, strideW):
+            if x + width > w:
+                x = w - width
+            tiles.append(image[y:y + height, x:x + width])
+    return tiles
 
-def save_numpy_tiles(path2slides, folder, slidename, coords, output_path, scale_factor):
+def save_numpy_tiles(path2slides, folder, slidename, output_path, scale_factor, height, width, strideH, strideW):
     slide_path = os.path.join(path2slides, folder, slidename)
     slide_name = os.path.basename(slidename).replace('.svs', '')
     slide = openslide.OpenSlide(slide_path)
-    zoom = openslide.deepzoom.DeepZoomGenerator(slide, tile_size=224, overlap=0)
-    level = int(coords[0, 0])
-
-    for idx, coord in enumerate(coords):
-        tile = extract_tile_features(level, coord, zoom)
-        tile_rescaled = rescale_image(tile, scale_factor)
+    slide_rescaled = rescale_image(slide, scale_factor)
+    slide_crop = crop_image(slide_rescaled, height, width, strideH, strideW)
+    
+    for idx, tile in enumerate(slide_crop):
         tile_filename = f"{slide_name}_{idx + 1:04d}.npy"
         tile_path = os.path.join(output_path, tile_filename)
-        np.save(tile_path, tile_rescaled)
+        np.save(tile_path, tile)
         print(f"Saved rescaled tile {tile_filename}")
+        
 
-def process_all_slides(path2slides, tile_coords, output_path, scale_factor):
+def process_all_slides(path2slides, output_path, scale_factor, height, width, strideH, strideW):
     slide_dirs = [d for d in os.listdir(path2slides) if os.path.isdir(os.path.join(path2slides, d))]
 
     slidenames = []
@@ -88,25 +103,20 @@ def process_all_slides(path2slides, tile_coords, output_path, scale_factor):
         os.mkdir(output_path)
 
     for folder, slidename in zip(subfolders, slidenames):
-        if slidename in tile_coords.keys():
-            output_folder = os.path.join(output_path, folder)
-            if not os.path.exists(output_folder):
-                os.mkdir(output_folder)
-            save_numpy_tiles(path2slides, folder, slidename, tile_coords[slidename], output_folder, scale_factor)
-        else:
-            print(f'Warning: tile coordinates not found for file {slidename}, skipping it')
-
+        output_folder = os.path.join(output_path, folder)
+        if not os.path.exists(output_folder):
+            os.mkdir(output_folder)
+        save_numpy_tiles(path2slides, folder, slidename, output_folder, scale_factor, height, width, strideH, strideW)
+      
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process TCGA WSI slides")
-    parser.add_argument('--input_dir', type=str, required=True, help="Path to the input directory containing WSI files")
-    parser.add_argument('--coords_file', type=str, required=True, help="Path to the file containing tile coordinates")
-    parser.add_argument('--output_dir', type=str, required=True, help="Path to the output directory to save tiles")
-
+    parser.add_argument('--input_dir', type = str, required = True, help = "Path to the input directory containing WSI files")
+    parser.add_argument('--output_dir', type = str, required = True, help = "Path to the output directory to save tiles")
     args = parser.parse_args()
-
-    # Load tile coordinates
-    with open(args.coords_file, 'rb') as f:
-        tile_coords = pickle.load(f)
-
-    scale_factor = 0.42 / 0.5
-    process_all_slides(args.input_dir, tile_coords, args.output_dir, scale_factor)
+    data = pd.read_csv('TCGA_scale.csv') # !!!!!!!change this working directory to the actual file location!
+    scale_factor = 0.42 / data['Scale.X'] # each one divided by it's on scale; not a fixed number!
+    height = 1536
+    width = 2048
+    strideH = 384
+    strideW = 512
+    process_all_slides(args.input_dir, args.output_dir, scale_factor, height, width, strideH, strideW)
